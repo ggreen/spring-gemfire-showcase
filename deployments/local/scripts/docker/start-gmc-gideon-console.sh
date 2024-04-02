42f7a729a91c351716a758aeb1c4eb3cd23b05ae
ggreen <gregoryg@vmware.com>
2024-04-02 14:38:39 -0400

    adding docker login

--- deployments/local/scripts/docker/start-gmc-gideon-console.sh
+++ deployments/local/scripts/docker/start-gmc-gideon-console.sh
@@ -1,1 +1,7 @@
+
+echo Login to registry.tanzu.vmware.com
+
+docker login registry.tanzu.vmware.com
+docker pull registry.tanzu.vmware.com/gemfire-management-console/gemfire-management-console:1.2.0
+
 docker run -d -p 8080:8080 --rm --name gideon-console   --network=gemfire-cache registry.tanzu.vmware.com/gemfire-management-console/gemfire-management-console:1.2.0
